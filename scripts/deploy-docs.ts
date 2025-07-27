e1877c3a85756d86142661ef80a8a14cfb446356
ben <ben76020567@gmail.com>
2025-07-27 23:19:14 +0800

    upd: docs deploy

--- scripts/deploy-docs.ts
+++
@@ -1,9 +0,0 @@
-import { execSync } from "child_process";
-
-execSync("vitepress build docs", { stdio: "inherit" });
-execSync(
-  'cd docs/.vitepress/dist && git init && git add . && git commit -m "deploy docs"',
-);
-execSync(
-  "cd docs/.vitepress/dist && git push -f git@github.com:empackjs/empack.git main:github-pages",
-);
