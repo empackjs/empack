d10f9ccc8bd3c017c36e57c3c0be46d47a5b56c5
ben <ben76020567@gmail.com>
2025-08-07 15:16:20 +0800

    upd: packages

--- packages/core/di/index.ts
+++ packages/core/di/index.ts
@@ -1,8 +1,5 @@
-export {
-  inject as Inject,
-  injectFromBase as InjectFromBase,
-  Newable,
-} from "inversify";
+export { inject as Inject, injectFromBase as InjectFromBase } from "inversify";
+export type { Newable } from "inversify";
 export type { BindingScope } from "./types";
 export { ContainerModule } from "./di";
 export { Injectable } from "./decoratoer";
--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,6 +1,3 @@
 export * from "./jwt";
-export * from "./logger";
 export * from "./result";
 export * from "./timer";
-export * from "./uuid";
-export * from "./validator";
